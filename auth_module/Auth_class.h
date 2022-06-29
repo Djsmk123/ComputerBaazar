c119ba199a6f6deb772fa839af059a1099fe7b16
MD Mobin <74850678+Djsmk123@users.noreply.github.com>
2022-06-29 14:05:28 +0530

    Update Auth_class.h

--- auth_module/Auth_class.h
+++ auth_module/Auth_class.h
@@ -58,7 +58,7 @@ void forgot_password(){
             file<<"Do not reply this email as it is auto generated.\nYour password is: "<<i["password"];
             file<<"\nThank You for using our service";
             file.close();
-            string s= "curl --ssl-reqd   --url smtps://smtp.gmail.com:465   --user computerbazarbot74804@gmail.com:PdC35qcGgyX46H   --mail-from computerbazarbot74804@gmail.com  --mail-rcpt ";
+            string s= "curl --ssl-reqd   --url smtps://smtp.gmail.com:465   --useruouremail:password   --mail-from youremail  --mail-rcpt ";
             s+=email;
             s+="   --upload-file mail.txt"; 
              system(s.c_str());
